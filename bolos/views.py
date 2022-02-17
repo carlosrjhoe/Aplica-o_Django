d60d3f22d6967b598cfcba5212cc1043e92b1736
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-16 21:07:56 -0300

    Puxando arquivos auxiliares para criação do site bolos da may

--- bolos/views.py
+++ bolos/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse('<h1>Bolos da May</h1><h2>Bem vindos aos melhores bolos da região</h2>')
+    return render(request,'index.html')
