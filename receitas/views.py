fe6c38684424ee603748664802877fcb898f837d
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-24 22:17:01 -0300

    Adicionando uma funcionalidade para adicionar uma nova receita dinamicamente

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1:'Bolo_vucão',
+        2:'Bolo_tipico',
+        3:'Broa',
+        4:'Bolo de chocolate'
+    }
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
