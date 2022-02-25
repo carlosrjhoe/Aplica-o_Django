6b91a916f5c7962246b0b874793d2da26c84096f
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-25 19:47:39 -0300

    Instalando o postgreSQL na aplicação

--- alurareceitas/settings.py
+++ alurareceitas/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'alurareceitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgrespl',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD': 'laracroft360',
+        'HOST': 'localhost'
     }
 }
 
