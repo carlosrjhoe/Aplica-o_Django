3cf604df7562db49b45fecded45717e027e5f9b6
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-28 19:53:00 -0300

    Melhorando a funcionalidade do site

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,5 +1,13 @@
+from re import search
 from django.contrib import admin
 from .models import Receita
 
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'date_receita')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 2
+
 # Register your models here.
-admin.site.register(Receita)
+admin.site.register(Receita, ListandoReceitas)
