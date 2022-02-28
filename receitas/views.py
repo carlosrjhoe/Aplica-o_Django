90cea3edab4d4929802a5f10524569f0c831dde6
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-28 20:59:04 -0300

    Adiconando Filtro em receitas publicadas

--- receitas/views.py
+++ receitas/views.py
@@ -3,7 +3,7 @@ from .models import Receita
 
 # Create your views here.
 def index(request):
-    receitas = Receita.objects.all()
+    receitas = Receita.objects.filter(publicada=True)
     dados = {
         'receitas' : receitas
     }
