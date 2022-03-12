a47be3bd077b3789df042b3cc8d128933398b9df
Carlos Roberto <carlosrjhoe@gmail.com>
2022-03-12 12:39:43 -0300

    encontrando e concertando um bug

--- usuarios/views.py
+++ usuarios/views.py
@@ -72,7 +72,7 @@ def dashboard(request):
 def cria_receita(request):
     if request.method == 'POST':
         nome_receita = request.POST['nome_receita']
-        #ingredientes = request.POST['ingredientes']
+        ingredientes = request.POST['ingredientes']
         modo_preparo = request.POST['modo_preparo']
         tempo_preparo = request.POST['tempo_preparo']
         rendimento = request.POST['rendimento']
@@ -80,7 +80,7 @@ def cria_receita(request):
         foto_receita = request.FILES['foto_receita']        
         
         user = get_object_or_404(User, pk=request.user.id)
-        receita = Receita.objects.create(pessoa=user, nome_receita=nome_receita, modo_preparo=modo_preparo, tempo_preparo=tempo_preparo, rendimento=rendimento, categoria=categoria, foto_receita=foto_receita)
+        receita = Receita.objects.create(pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes, modo_preparo=modo_preparo, tempo_preparo=tempo_preparo, rendimento=rendimento, categoria=categoria, foto_receita=foto_receita)
         
         receita.save()
         return redirect('dashboard')
